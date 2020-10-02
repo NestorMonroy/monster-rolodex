558db10ccb790857942363a2aeeaf004a112a2f5
NestorMonroy <nestor.monroy.90@gmail.com>
2020-10-02 13:48:28 -0500

    finishing the react example

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
 
     return (
       <div className="App">
-          <h1>Monsters Rolodex</h1>
+          <h1>Cats </h1>
           <SearchBox 
             placeholder='search monsters'
             onSearchChange={this.onSearchChange} 
